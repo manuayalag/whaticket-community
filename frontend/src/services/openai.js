d4a60a9f9b3e172c1e44cdf374c28bfe4617aae7
Manuel Ayala <cesarayala@fiuni.edu.py>
2025-06-13 15:24:35 -0300

    solucion a unauthorized?

--- frontend/src/services/openai.js
+++ frontend/src/services/openai.js
@@ -1,8 +1,4 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-});
+import api from "./api";
 
 export const getOpenAISettings = async () => {
   const { data } = await api.get("/openai/settings");
