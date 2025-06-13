c1ded128a712150fd8f2a9f14d7c4aad3d0b7d13
Manuel Ayala <cesarayala@fiuni.edu.py>
2025-06-13 18:08:15 -0300

    loggerr

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -2,7 +2,22 @@ import pino from "pino";
 
 const logger = pino({
   enabled: true,
-  level: "debug"
+  level: "debug",
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      levelFirst: true,
+      translateTime: "SYS:standard",
+    }
+  },
+  timestamps: true,
+  base: undefined,
+  formatters: {
+    level: (label) => {
+      return { level: label };
+    }
+  }
 });
 
 export { logger };
